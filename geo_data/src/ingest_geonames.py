4eb88a6ea21e30dc5093d343ee098c6ad5a43523
manekiyong <59855081+manekiyong@users.noreply.github.com>
2022-09-05 18:08:52 +0800

    geodata

---
+++ geo_data/src/ingest_geonames.py
@@ -0,0 +1,47 @@
+from elasticsearch import Elasticsearch, RequestError
+import pandas as pd
+import yaml
+from tqdm import tqdm
+
+
+def read_yaml(file_path='config_f1.yaml'):
+    with open(file_path, "r") as f:
+        return yaml.safe_load(f)
+
+config = read_yaml()
+
+ELASTIC_URL = config['ELASTICSEARCH']['URL']
+INDEX_NAME = config['ELASTICSEARCH']['INDEX_NAME']
+ELASTIC_USERNAME = config['ELASTICSEARCH']['ELASTIC_USERNAME']
+ELASTIC_PASSWORD = config['ELASTICSEARCH']['ELASTIC_PASSWORD']
+
+client = Elasticsearch(ELASTIC_URL,  # ca_certs="",
+                       verify_certs=False,
+                       basic_auth=(ELASTIC_USERNAME, ELASTIC_PASSWORD))
+
+
+def es_create_index_if_not_exists(es, index):
+    """Create the given ElasticSearch index and ignore error if it already exists"""
+    try:
+        es.indices.create(index=index)
+    except RequestError as ex:
+        if ex.error == 'resource_already_exists_exception':
+            print("Index already exists!!")
+            pass # Index already exists. Ignore.
+        else: # Other exception - raise it
+            raise ex
+
+if __name__ == '__main__':
+
+    client.indices.create(index=INDEX_NAME)
+    col_headers = ['geonameid', 'name', 'asciiname', 'alternatenames', 'latitude', 'longitude', 'feature_class', 'feature_code', 'country_code', 'cc2', 'admin1_code', 'admin2_code', 'admin3_code', 'admin4_code', 'population', 'elevation', 'dem', 'timezone', 'modification_date']
+    df = pd.read_csv('allCountries.txt', sep='\t',names=col_headers)
+
+    # Check if indices is create, if yes throw exception
+    es_create_index_if_not_exists(client, INDEX_NAME)
+
+    for i, rows in tqdm(df.iterrows()):
+        loc_dict = rows.dropna().to_dict()
+        geo_id = loc_dict['geonameid']
+        loc_dict.pop('geonameid', None)
+        client.index(index=INDEX_NAME, document=loc_dict, id=str(geo_id))

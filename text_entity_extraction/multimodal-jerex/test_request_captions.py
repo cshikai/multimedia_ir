71d652960de808a0787611795ca3b19050a92be9
ShearmanChua <shearman_chua@hotmail.com>
2022-09-16 09:03:42 +0800

    new

--- text_entity_extraction/multimodal-jerex/test_request_captions.py
+++ text_entity_extraction/multimodal-jerex/test_request_captions.py
@@ -37,67 +37,6 @@ def predict_blink(dataset: Dict):
     return df
 
 
-def generate_entity_linking_df(results_df):
-
-    entities_linking_df = pd.DataFrame(
-        columns=['doc_id', 'mention', 'mention_type', 'context_left', 'context_right'])
-
-    for idx, row in results_df.iterrows():
-        doc_id = row['doc_id']
-        if type(row['relations']) == str:
-            relations = ast.literal_eval(row['relations'])
-            tokens = ast.literal_eval(row['tokens'])
-        else:
-            relations = row['relations']
-            tokens = row['tokens']
-        entities = []
-        for relation in relations:
-            head_entity = " ".join(
-                tokens[relation['head_span'][0]:relation['head_span'][1]])
-            if head_entity not in entities:
-                print("Head Entity:")
-                print(head_entity)
-                left_context = " ".join(
-                    tokens[relation['head_span'][0]-100:relation['head_span'][0]])
-                left_context = re.sub(r"\S*https?:\S*", '', left_context)
-                print("Left context: ", left_context)
-                print("\n")
-                right_context = " ".join(
-                    tokens[relation['head_span'][1]:relation['head_span'][1]+100])
-                right_context = re.sub(r"\S*https?:\S*", '', right_context)
-                print("Right context: ", right_context)
-                print("\n")
-                entities_linking_df.loc[-1] = [doc_id, head_entity,
-                                               relation['head_type'], left_context, right_context]  # adding a row
-                entities_linking_df.index = entities_linking_df.index + 1  # shifting index
-                entities_linking_df = entities_linking_df.sort_index()  # sorting by index
-                entities.append(head_entity)
-
-            tail_entity = " ".join(
-                tokens[relation['tail_span'][0]:relation['tail_span'][1]])
-            if tail_entity not in entities:
-                print("Tail Entity:")
-                print(tail_entity)
-                left_context = " ".join(
-                    tokens[relation['tail_span'][0]-100:relation['tail_span'][0]])
-                left_context = re.sub(r"\S*https?:\S*", '', left_context)
-                print("Left context: ", left_context)
-                print("\n")
-                right_context = " ".join(
-                    tokens[relation['tail_span'][1]:relation['tail_span'][1]+100])
-                right_context = re.sub(r"\S*https?:\S*", '', right_context)
-                print("Right context: ", right_context)
-                print("\n")
-                entities_linking_df.loc[-1] = [doc_id, tail_entity,
-                                               relation['tail_type'], left_context, right_context]  # adding a row
-                entities_linking_df.index = entities_linking_df.index + 1  # shifting index
-                entities_linking_df = entities_linking_df.sort_index()  # sorting by index
-                entities.append(tail_entity)
-
-    print(entities_linking_df.head())
-    return entities_linking_df
-
-
 def generate_entity_linking_df_entities(results_df):
 
     entities_linking_df = pd.DataFrame(columns=[
@@ -158,11 +97,10 @@ if __name__ == '__main__':
     documents = document_store.get_all_documents()
 
     articles_df = pd.DataFrame(
-        columns=['ID', 'title', 'text', 'elasticsearch_ID'])
+        columns=['ID','elasticsearch_ID','title','image_captions','images'])
 
     for document in documents:
-        articles_df.loc[-1] = [document.meta['ID'], document.meta['link'],
-                               document.content, document.id]  # adding a row
+        articles_df.loc[-1] = [document.meta['ID'], document.id, document.meta['link'], document.meta['image_captions'],document.meta['images']]  # adding a row
         articles_df.index = articles_df.index + 1  # shifting index
         articles_df = articles_df.sort_index()  # sorting by index
 
@@ -170,9 +108,22 @@ if __name__ == '__main__':
     #     'data/m2e2.csv')
     print(articles_df.info())
     print(articles_df.head())
-    # articles_df = articles_df[:10]
 
-    df_json = articles_df.to_json(orient="records")
+    exploded_df = pd.DataFrame(columns=['ID','elasticsearch_ID','title','text','images'])
+
+    for idx, row in articles_df.iterrows():
+        doc_id = row['ID']
+        es_id = row['elasticsearch_ID']
+        title = row['title']
+        for idx in range(0,len(row['images'])):
+            exploded_df.loc[-1] = [doc_id,es_id,title,row['image_captions'][idx],row['images'][idx]]  # adding a row
+            exploded_df.index = exploded_df.index + 1  # shifting index
+            exploded_df = exploded_df.sort_index()  # sorting by index
+
+    print(exploded_df.info())
+    print(exploded_df.head())
+
+    df_json = exploded_df.to_json(orient="records")
     df_json = json.loads(df_json)
     jerex_results = predict_jerex(df_json)
     print("jerex results: ", jerex_results)
@@ -225,60 +176,60 @@ if __name__ == '__main__':
                       == "Unknown", "entity_link"] = str(-1)
     # blink_results["entity_link"] = pd.to_numeric(blink_results["entity_link"])
 
-    list_of_cluster_dfs = blink_results.groupby('doc_id')
+    # list_of_cluster_dfs = blink_results.groupby('doc_id')
 
-    entities = []
-    ids = []
+    # entities = []
+    # ids = []
 
-    for group, cluster_df in list_of_cluster_dfs:
-        doc_entities = []
-        doc_id = cluster_df['doc_id'].tolist()[0]
-        mentions = cluster_df['mention'].tolist()
-        cluster_df['char_spans'] = cluster_df['char_spans'].apply(
-            lambda list_string: ast.literal_eval(list_string))
-        mention_spans = cluster_df['char_spans'].tolist()
-        mention_spans = [tuple(span) for span in mention_spans]
-        mentions_type = cluster_df['mention_type'].tolist()
-        entity_links = cluster_df['entity_link'].tolist()
-        entity_names = cluster_df['entity_names'].tolist()
+    # for group, cluster_df in list_of_cluster_dfs:
+    #     doc_entities = []
+    #     doc_id = cluster_df['doc_id'].tolist()[0]
+    #     mentions = cluster_df['mention'].tolist()
+    #     cluster_df['char_spans'] = cluster_df['char_spans'].apply(
+    #         lambda list_string: ast.literal_eval(list_string))
+    #     mention_spans = cluster_df['char_spans'].tolist()
+    #     mention_spans = [tuple(span) for span in mention_spans]
+    #     mentions_type = cluster_df['mention_type'].tolist()
+    #     entity_links = cluster_df['entity_link'].tolist()
+    #     entity_names = cluster_df['entity_names'].tolist()
 
-        for idx in range(0, len(mentions)):
-            mention = dict()
-            mention['mention'] = mentions[idx]
-            mention['mention_type'] = mentions_type[idx]
-            mention['mention_span'] = mention_spans[idx]
-            mention['entity_link'] = entity_links[idx]
-            mention['entity_name'] = entity_names[idx]
-            doc_entities.append(mention)
-        ids.append(doc_id)
-        entities.append(doc_entities)
+    #     for idx in range(0, len(mentions)):
+    #         mention = dict()
+    #         mention['mention'] = mentions[idx]
+    #         mention['mention_type'] = mentions_type[idx]
+    #         mention['mention_span'] = mention_spans[idx]
+    #         mention['entity_link'] = entity_links[idx]
+    #         mention['entity_name'] = entity_names[idx]
+    #         doc_entities.append(mention)
+    #     ids.append(doc_id)
+    #     entities.append(doc_entities)
 
-    entities_df = pd.DataFrame()
-    entities_df['ID'] = ids
-    entities_df['identified_entities'] = entities
+    # entities_df = pd.DataFrame()
+    # entities_df['ID'] = ids
+    # entities_df['identified_entities'] = entities
 
-    print(entities_df.head())
+    # print(entities_df.head())
 
-    results_df = pd.merge(articles_df, entities_df, how="left", on=["ID"])
+    # results_df = pd.merge(articles_df, entities_df, how="left", on=["ID"])
 
-    template_mention = dict()
-    template_mention['mention'] = ""
-    template_mention['mention_type'] = ""
-    template_mention['mention_span'] = []
-    template_mention['entity_link'] = ""
-    template_mention['entity_name'] = ""
-    template_entity = [mention]
+    # template_mention = dict()
+    # template_mention['mention'] = ""
+    # template_mention['mention_type'] = ""
+    # template_mention['mention_span'] = []
+    # template_mention['entity_link'] = ""
+    # template_mention['entity_name'] = ""
+    # template_entity = [mention]
 
-    results_df = results_df.fillna(value={"identified_entities": []})
+    # results_df = results_df.fillna(value={"identified_entities": []})
 
-    print(results_df.info())
-    results_df.to_csv("data/jerex_plus_blink.csv", index=False)
+    # print(results_df.info())
+    # results_df.to_csv("data/jerex_plus_blink.csv", index=False)
 
-    # Update results to ElasicSearch
-    for idx, row in results_df.iterrows():
-        meta_dict = {'text_entities': row['identified_entities']}
-        document_store.update_document_meta(
-            id=row['elasticsearch_ID'], meta=meta_dict)
+    # # Update results to ElasicSearch
+    # for idx, row in results_df.iterrows():
+    #     meta_dict = {'text_entities': row['identified_entities']}
+    #     document_store.update_document_meta(
+    #         id=row['elasticsearch_ID'], meta=meta_dict)
 
-    end = time.time()
+    # end = time.time()
     print("Time to complete jerex and entity linking", end - start)

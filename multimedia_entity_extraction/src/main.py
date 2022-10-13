fdec4a2b3271c04e928e6aa44434b8b0bffb9909
manekiyong <59855081+manekiyong@users.noreply.github.com>
2022-10-13 12:13:54 +0800

    [vg] main

---
+++ multimedia_entity_extraction/src/main.py
@@ -0,0 +1,31 @@
+from inference_api.visual_attention.manager import VAManager
+from inference_api.visual_attention.log_manager import VALogDatabaseManager
+
+from elasticsearch import Elasticsearch
+
+ELASTIC_URL = "https://elasticsearch:9200"
+client = Elasticsearch(ELASTIC_URL,
+                                    basic_auth=('elastic', 'changeme'),
+                                    verify_certs=False
+                                    )
+
+log_manager = VALogDatabaseManager('visual_attention_caption')
+va_manager = VAManager(log_manager)
+
+
+def download_image_b64(server_path):
+    body = {'server_path': server_path}
+    r = requests.get('http://image_server:8000/download/', json=body)
+    return r.json()['image']
+
+def ledger():
+    pass
+
+def rollback():
+    pass
+
+
+if __name__ == "__main__":
+    output, text_entity_id_list, visual_entity_id_list = va_manager.infer([article_id])
+
+## To return: bbox, 

736c256e9a9024fdf8563107de976683e5bbf9c4
ShearmanChua <shearman_chua@hotmail.com>
2022-11-28 11:26:43 +0800

    idk

--- text_entity_extraction/multimodal-jerex/test_request_captions.py
+++ text_entity_extraction/multimodal-jerex/test_request_captions.py
@@ -153,7 +153,7 @@ if __name__ == '__main__':
     #     else:
     #         entities = row['entities']
 
-    #     for entity_row in entities:
+    #     for entity_row in entities: 
     #         count += len(entity_row)
 
     # print("total number of entities: ", count)
